19aa54965e01375279cb5f6782256876fe22281a
yunhyun <skingsol@naver.com>
2023-03-27 18:01:18 +0900

    EmpDAO,EmpMain 수정

--- database/src/database2/EmpDAO.java
+++ database/src/database2/EmpDAO.java
@@ -146,4 +146,56 @@ public class EmpDAO {
 		return status;
 	}
 	
+	//사원 삭제
+	public boolean remove(int empno) {
+		boolean status = false;
+		
+		
+		try {
+			con = getcoConnection();
+			//empno 일치시 사원 삭제
+			String sql = "DELETE FROM emp_temp WHERE empno=? ";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, empno);
+			
+			int result = pstmt.executeUpdate();
+			
+			if (result > 0) status = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(con, pstmt);
+		}
+		return status;
+	}
+	
+	//사원추가
+	public boolean insert(EmpDTO empDTO) {
+		boolean status = false;
+		try {
+			con = getcoConnection();
+			
+			String sql = "INSERT INTO emp_temp(empno, ename, job, mgr, hiredate, sal, comm, deptno) VALUES(?,?,?,?,sysdate,?,?,?) ";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, empDTO.getEmpno());
+			pstmt.setString(2, empDTO.getEname());
+			pstmt.setString(3, empDTO.getJob());
+			pstmt.setInt(4, empDTO.getMgr());
+			pstmt.setInt(5, empDTO.getSal());
+			pstmt.setInt(6, empDTO.getComm());
+			pstmt.setInt(7, empDTO.getDeptno());
+			
+			int result = pstmt.executeUpdate();
+			
+			if(result > 0) status = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(con, pstmt);
+		}
+		return status;
+	}
 }
--- database/src/database2/EmpMain.java
+++ database/src/database2/EmpMain.java
@@ -27,18 +27,39 @@ public class EmpMain {
 			// switch 작성
 			switch (menu) {
 			case 1:
-				System.out.print("사원명 입력");
-				String name = sc.nextLine();
+				EmpDTO empDTO = new EmpDTO();
+				System.out.println("추가할 사원정보 입력");
+				System.out.print("사원번호 입력 >>");
+				empDTO.setEmpno(Integer.parseInt(sc.nextLine()));
+				System.out.print("사원명 입력 >>");
+				empDTO.setEname(sc.nextLine());
+				System.out.print("직무 입력 >>");
+				empDTO.setJob(sc.nextLine());
+				System.out.print("매니저번호 입력 >>");
+				empDTO.setMgr(Integer.parseInt(sc.nextLine()));
+				System.out.print("급여 입력 >>");
+				empDTO.setSal(Integer.parseInt(sc.nextLine()));
+				System.out.print("추가수당 입력 >>");
+				empDTO.setComm(Integer.parseInt(sc.nextLine()));
+				System.out.print("부서번호 입력 >>");
+				empDTO.setDeptno(Integer.parseInt(sc.nextLine()));
+				
+				System.out.println(empDAO.insert(empDTO)?"사원 정보 추가 성공":"사원정보 추가 실패");
 				
 				break;
 			case 2:
+				//삭제할 empno 입력받기
+				System.out.println("삭제할 사원번호 입력");
+				System.out.print("사원번호 >> ");
+				int empno = Integer.parseInt(sc.nextLine());
+				System.out.println(empDAO.remove(empno)?"사원 번호 삭제 성공":"사원번호 삭제 실패");
 				
 				break;
 			case 3:
 				//empno, sal 입력받기
 				System.out.println("수정할 정보 입력");
 				System.out.print("사원번호 >> ");
-				int empno = Integer.parseInt(sc.nextLine());
+				empno = Integer.parseInt(sc.nextLine());
 				
 				System.out.print("수정급여 >> ");
 				int sal = Integer.parseInt(sc.nextLine());
@@ -81,11 +102,11 @@ public class EmpMain {
 				}else {
 					System.out.println("사번\t사원명\t직무\t입사일");
 					System.out.println("--------------------------");
-					for (EmpDTO empDTO : list) {
-						System.out.print(empDTO.getEmpno()+"\t");
-						System.out.print(empDTO.getEname()+"\t");
-						System.out.print(empDTO.getJob()+"\t");
-						System.out.print(empDTO.getHiredate()+"\n\n");
+					for (EmpDTO empDTO1 : list) {
+						System.out.print(empDTO1.getEmpno()+"\t");
+						System.out.print(empDTO1.getEname()+"\t");
+						System.out.print(empDTO1.getJob()+"\t");
+						System.out.print(empDTO1.getHiredate()+"\n\n");
 				}
 			}
 				break;
